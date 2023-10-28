a17920ebe2eec1b485b6013f545af4d378f515ef
William-xr4 <108703320+William-xr4@users.noreply.github.com>
2023-10-28 10:07:32 -0300

    Detector de Objetos Web-D.O.W.
    Hospedando meu site!

---
+++ main.js
@@ -0,0 +1,41 @@
+var status="";
+
+
+function preload(){
+    img=loadImage("cars.jpg");
+}
+
+function setup(){
+    canvas=createCanvas(1000, 600);
+    canvas.center();
+    detect_object=ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("Btn_status").innerHTML="Status: Detectando objeto(s)";
+}
+
+function modelLoaded(){
+    console.log("Modelo carregado");
+    status=true;
+    detect_object.detect(img, gotResults);
+}
+
+function gotResults(error, results){
+    if(error){
+        console.error(error);
+    }
+        console.log(results);
+        objects=results;
+}
+
+function draw(){
+    image(img, 0, 0, 1000, 600);
+    if(status != ""){
+        for(var inc=0; inc<objects.length; inc++){
+            document.getElementById("Btn_status").innerHTML="status:Objeto(s) detectado(s).";
+            fill("#00FFFF");
+            percent=floor(objects[inc].confidence*100);
+            text(objects[inc].label+" "+percent+"%", objects[inc].x, objects[inc].y);
+            noFill();
+            rect(objects[inc].x, objects[inc].y, objects[inc].width, objects[inc].height);
+        }
+    }
+}
